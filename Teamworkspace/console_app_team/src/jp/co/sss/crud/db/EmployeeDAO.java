5708108b9c3a4312d7a8fbb79af6a694904343b1
三輪陽 <h-miwa@k-shared.co.jp>
2025-08-21 13:33:35 +0900

    変更

--- Teamworkspace/console_app_team/src/jp/co/sss/crud/db/EmployeeDAO.java
+++ Teamworkspace/console_app_team/src/jp/co/sss/crud/db/EmployeeDAO.java
@@ -194,7 +194,7 @@ public class EmployeeDAO {
 		 String gender=br.readLine();
 		System.out.print("生年月日（西暦年/月/日）:");
 		 String birthday=br.readLine();
-		 System.out.print("部署ID(1：営業部、2：経理部、3：総務部)");
+		 System.out.print("部署ID(1：営業部、2：経理部、3：総務部):");
 		 String deptId=br.readLine();
 		 
 		 connection = DBManager.getConnection();
